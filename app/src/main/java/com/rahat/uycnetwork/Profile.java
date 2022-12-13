c337a44435097167ec978bbe5b5580f0c4419b11
Nahid <nahidhasan24>
2022-12-13 22:51:39 +0600

    Initial commit

---
+++ app/src/main/java/com/rahat/uycnetwork/Profile.java
@@ -0,0 +1,14 @@
+package com.rahat.uycnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
